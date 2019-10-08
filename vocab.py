7c2a29c1772547ee4e5125359bae60f8cb00e722
Lane Schwartz <dowobeha@gmail.com>
2019-10-08 11:42:34 -0500

    Added main to create vocab in pickle format

--- vocab.py
+++ vocab.py
@@ -6,26 +6,38 @@ class VocabularyEntry(NamedTuple):
     string: str
 
 
-class CoreVocabulary(NamedTuple):
+class ReservedSymbols(NamedTuple):
     pad: VocabularyEntry
     oov: VocabularyEntry
     start_of_sequence: VocabularyEntry
     end_of_sequence: VocabularyEntry
 
 
-class Vocabulary(CoreVocabulary):
+class Vocabulary:
 
-    def __new__(cls, *,
-                pad: VocabularyEntry,
-                oov: VocabularyEntry,
-                start_of_sequence: VocabularyEntry,
-                end_of_sequence: VocabularyEntry,
-                entry_list: List[VocabularyEntry],
-                entry_map: Mapping[Union[int, str], VocabularyEntry]):
-        self = super(Vocabulary, cls).__new__(cls, pad, oov, start_of_sequence, end_of_sequence)
+    def __init__(self, *,
+                 reserved_symbols: ReservedSymbols,
+                 entry_list: List[VocabularyEntry],
+                 entry_map: Mapping[Union[int, str], VocabularyEntry]):
+        self._reserved_symbols = reserved_symbols
         self._entry_list = entry_list
         self._entry_map = entry_map
-        return self
+
+    @property
+    def oov(self) -> VocabularyEntry:
+        return self._reserved_symbols.oov
+
+    @property
+    def pad(self) -> VocabularyEntry:
+        return self._reserved_symbols.pad
+
+    @property
+    def start_of_sequence(self) -> VocabularyEntry:
+        return self._reserved_symbols.start_of_sequence
+
+    @property
+    def end_of_sequence(self) -> VocabularyEntry:
+        return self._reserved_symbols.end_of_sequence
 
     def __len__(self):
         return len(self._entry_list)
@@ -64,16 +76,16 @@ class Vocabulary(CoreVocabulary):
         e = VocabularyEntry(integer=3, string=end_of_sequence)
 
         entry_map: MutableMapping[Union[int, str], VocabularyEntry] = {0: p, pad: p,
-                     1: o, oov: o,
-                     2: s, start_of_sequence: s,
-                     3: e, end_of_sequence: e}
+                                                                       1: o, oov: o,
+                                                                       2: s, start_of_sequence: s,
+                                                                       3: e, end_of_sequence: e}
 
         entry_list = [p, o, s, e]
 
         for string in strings:
 
             if string == pad or string == oov or string == start_of_sequence or string == end_of_sequence:
-                raise ValueError(f"Strings must not contain reserved string:\t{s}")
+                raise ValueError(f"Strings must not contain reserved string:\t{string}")
 
             if string not in entry_map:
                 v = VocabularyEntry(integer=len(entry_list), string=string)
@@ -81,23 +93,35 @@ class Vocabulary(CoreVocabulary):
                 entry_map[v.string] = v
                 entry_list.append(v)
 
-        return Vocabulary(pad=p,
-                          oov=o,
-                          start_of_sequence=s,
-                          end_of_sequence=e,
+        reserved: ReservedSymbols = ReservedSymbols(pad=p, oov=o, start_of_sequence=s, end_of_sequence=e)
+
+        return Vocabulary(reserved_symbols=reserved,
                           entry_list=entry_list,
                           entry_map=entry_map)
 
 
 if __name__ == '__main__':
 
-    vocab = Vocabulary.construct(pad='<pad>',
-                                 oov='<oov>',
-                                 start_of_sequence='<s>',
-                                 end_of_sequence='</s>',
-                                 strings=['a', 'b', 'oov', 'c'])
+    import pickle
+    import sys
 
-    print(vocab.oov in vocab)
-    print(None in vocab)
-    print(8 in vocab)
-    print(len(vocab))
+    if len(sys.argv) != 3:
+        print(f"Usage:\t{sys.argv[0]} one_symbol_per_line.txt vocab.pkl")
+        sys.exit(-1)
+
+    with open(sys.argv[1]) as symbol_file, open(sys.argv[2], 'wb') as pickle_file:
+
+        print(f"Reading symbols from {sys.argv[1]}...", file=sys.stderr)
+        symbols = [line.strip() for line in symbol_file]
+
+        print(f"Constructing vocabulary from symbols...", file=sys.stderr)
+        vocab = Vocabulary.construct(pad='<pad>',
+                                     oov='<oov>',
+                                     start_of_sequence='<s>',
+                                     end_of_sequence='</s>',
+                                     strings=symbols)
+
+        print(f"Writing vocabulary of {len(vocab)} symbols to {sys.argv[2]}...", file=sys.stderr)
+        pickle.dump(vocab, pickle_file)
+
+        print(f"Completed writing vocabulary of {len(vocab)} symbols to {sys.argv[2]}", file=sys.stderr)
